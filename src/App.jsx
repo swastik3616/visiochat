3e01ad68d4cb45fd71e07f2b4f332e3dd244b503
Swastik Kulkarni <swastikkulkarni737@gmail.com>
2026-05-04 12:37:15 +0530

    Fix the main file

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Splash from "./pages/Splash"
-import Login from "./pages/Login"
-import Signup from "./pages/Signup"
-import Chats from "./pages/Chats"
-import AdminChat from "./pages/AdminChat"
-import MemberChat from "./pages/MemberChat"
 
-export default function App() {
+function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Splash />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/chats" element={<Chats />} />
-        <Route path="/chat/admin/:groupId" element={<AdminChat />} />
-        <Route path="/chat/member/:groupId" element={<MemberChat />} />
       </Routes>
     </BrowserRouter>
   )
 }
+
+export default App
